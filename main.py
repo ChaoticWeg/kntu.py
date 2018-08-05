c22065887df312506ab1929caa85612931b3ae1f
ChaoticWeg <shawn@chaoticweg.cc>
2018-08-05 18:03:07 -0500

    add .env support

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from dotenv import load_dotenv
+import os
+
 def run():
-    print("Hello, world!")
+    token = os.getenv("DISCORD_BOT_TOKEN")
+    print(token)
 
 if __name__ == "__main__":
+    load_dotenv()
     run()
